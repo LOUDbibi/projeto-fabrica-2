e4dd38925019b0e8cbfb8fbc3a51c7695228e950
Ana Beatriz <ab12348ff@gmail.com>
2025-09-09 16:54:26 -0300

    Calculando valor de salario

---
+++ Salario.py
@@ -0,0 +1,4 @@
+valor_hora= float(input("Digite o valor de hora"))
+horas_mes=int(input("Digite o valar de horas trabalhadas"))
+salario= valor_hora * horas_mes
+print(f" O valor de salario é {salario:.2f}")
